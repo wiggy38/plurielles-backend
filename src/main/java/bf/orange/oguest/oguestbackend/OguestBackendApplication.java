1f4627c91f19a5e41a517ec462d22b03c125339a
wiggy <m.miguellao@gmail.com>
2022-05-20 12:21:04 +0000

    Initial commit

---
+++ src/main/java/bf/orange/oguest/oguestbackend/OguestBackendApplication.java
@@ -0,0 +1,13 @@
+package bf.orange.oguest.oguestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OguestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OguestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bf/orange/oguest/oguestbackend/OguestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bf.orange.oguest.oguestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OguestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
