0f5865de6c6de7357707ad745a5c6dbc937d57a6
Wiggy <35220596+wiggy38@users.noreply.github.com>
2022-06-15 11:27:51 +0500

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -25,7 +25,7 @@
 --
 
 -- INSERT INTO users (id, active, activetoken, displayname, email, nom, password, phone, prenoms, username) VALUES
-    (1, true, NULL, 'John DOE', 'miguel.lao@orange.com', 'DOE', '$2a$10$PM5nq1iZ.iFm7VSGlf2A2O3Y7rtUQKuI5TLU6a4xCbWbgu2BHhRFu', '74707070', 'John', 'HRVN5598');
+--    (1, true, NULL, 'John DOE', 'miguel.lao@orange.com', 'DOE', '$2a$10$PM5nq1iZ.iFm7VSGlf2A2O3Y7rtUQKuI5TLU6a4xCbWbgu2BHhRFu', '74707070', 'John', 'HRVN5598');
 
 -- --
 -- -- Déchargement des données de la table user_role
